2a27af23de94583f854a171d6a2630a77062210a
haechan <haechan2000@noutecompany.com>
2023-09-04 23:38:39 +0900

    feat: 인터페이스 2개 상속을 통해 역할과 기능에 제한을 둘 수 있는 추상화 방법 구현

--- abstraction/abstraction.ts
+++ abstraction/abstraction.ts
@@ -6,12 +6,21 @@
     hasMilk: boolean;
   };
 
+  // * 구현할 클래스의 명세를 작성한다.
   interface CoffeeMaker {
     makeCoffee(shots: number): CoffeeCup;
     fillCoffeeBeans(beans: number): void;
   }
 
-  class CoffeeMachine implements CoffeeMaker {
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
+  // * interface를 implementations한다.
+  // * interface 2개를 implementations할 수 있으며 2개의 명세를 모두 만족해야 한다.
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
     private static BEANS_GRAM_PER_SHOT = 7; // * private 키워드를 붙이면 외부에서 내부 멤버변수에 접근 불가
     protected coffeeBeans = 0; // * protected 키워드를 사용하면 private과 동일하게 여전히 외부에서 접근은 불가하지만, 해당 클래스를 상속받은 자식 클래스 내부에서는 접근이 가능하다.
     // * constructor에 대한 접근도 private으로 설정 해주는게 안전하다.
@@ -19,7 +28,11 @@
       this.coffeeBeans = coffeeBeans;
     }
 
-    static makeMachine(coffeeBeans: number): CoffeeMaker {
+    clean(): void {
+      console.log('cleaning the machine.');
+    }
+
+    static makeMachine(coffeeBeans: number) {
       return new CoffeeMachine(coffeeBeans);
     }
 
@@ -57,8 +70,39 @@
     }
   }
 
-  const maker = CoffeeMachine.makeMachine(32);
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
   // * private 키워드를 통해 정말 필요한 메소드만 외부에 노출되게 하는 방식으로 추상화를 할 수 있다.
   maker.fillCoffeeBeans(32);
   maker.makeCoffee(2);
+
+  const maker2: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
+  // * private 키워드를 통해 정말 필요한 메소드만 외부에 노출되게 하는 방식으로 추상화를 할 수 있다.
+  maker2.fillCoffeeBeans(32);
+  maker2.makeCoffee(2);
+  maker2.clean();
+
+  // * 어떤 인터페이스를 상속 받느냐에 따라 클래스의 역할과 기능에 제한을 둘 수 있다.
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log('ama', coffee);
+    }
+  }
+
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log('pro', coffee);
+      this.machine.fillCoffeeBeans(40);
+      this.machine.clean();
+    }
+  }
+
+  const machine = CoffeeMachine.makeMachine(50);
+  const ama = new AmateurUser(machine);
+  const pro = new ProBarista(machine);
+  ama.makeCoffee();
+  pro.makeCoffee();
 }
